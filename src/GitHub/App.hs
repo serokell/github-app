8452e3b00ebd05437496356fb1adf61c1261c028
Haskell-mouse <lazybonesxp@gmail.com>
2018-08-04 22:03:41 +0300

    [#1] Changes due to PR review: use UTCTime instead of POSIXTime, MVar, instead of IORef, Bytestring to store token, etc...

--- src/GitHub/App.hs
+++ src/GitHub/App.hs
@@ -1,38 +1,36 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module GitHub.App
-  (
-   , InstallationAuth 
-   , authenticateInstallation
-   , createInstToken
-  ) where
+       ( InstallationAuth 
+       , authenticateInstallation
+       , createInstAuth
+       ) where
 
-import Universum hiding          (exp, Option)
+import Universum hiding          (Option, exp)
 import Crypto.Types.PubKey.RSA   (PrivateKey(..))
 import Data.Aeson                (FromJSON (..), withObject, (.:))
 import Data.Default.Class        (def)
+import Data.Time                 (NominalDiffTime(..), UTCTime(..), addUTCTime, defaultTimeLocale
+                                 , diffUTCTime, getCurrentTime, iso8601DateFormat, parseTimeM)
+import Data.Time.Clock.POSIX     (utcTimeToPOSIXSeconds)
 import GitHub.Auth               (Auth (..))
 import Lens.Micro.Platform       (makeLenses, (^.))
 import Network.HTTP.Req          (POST (..), NoReqBody (..), Option, Url, header, https,
                                    jsonResponse, req, responseBody, runReq, (/:))
-import Data.Time.Clock           (UTCTime (..))
-import Data.Time.Clock.POSIX     (POSIXTime,getPOSIXTime, utcTimeToPOSIXSeconds)
 import Web.JWT                   (Signer(..), JSON, JWTClaimsSet(..), encodeSigned, numericDate, stringOrURI)
-import qualified Prelude as Show (Show, show)
-import qualified  Data.Text as T (takeWhileEnd, takeWhile)
 
 -- | Json Web Token expiration time. Maximun accepted by github is 10 minutes. 
-jwtExpTime :: POSIXTime
+jwtExpTime :: NominalDiffTime
 jwtExpTime = 600.0 
 
 -- | Installation key expiration time. It is fixed by github and is equal to 1 hour. 
-instKeyExpTime :: POSIXTime
+instKeyExpTime :: NominalDiffTime
 instKeyExpTime = 3600.0
 
 -- | Time preserved to be on the safe side. 
 -- if expiration time of installation auth token <= current time + bufferTime 
 -- then installation Auth token shoul be updated. 
-bufferTime :: POSIXTime 
+bufferTime :: NominalDiffTime
 bufferTime = instKeyExpTime * 0.25
 
 -- | base URL of api
@@ -43,95 +41,75 @@ type BaseURL = Text
 type InstallationId = Text 
 
 
--- | Github installation Auth token 
-data GTAToken = GTAToken 
-    {
-        instToken  :: !Text,      -- ^ Installation token 
-        expireAt   :: !POSIXTime  -- ^ expiration time 
-    }
+-- | Github installation access token
+data InstallationToken = InstallationToken
+    { getToken          :: ByteString
+    , getExpirationTime :: UTCTime
+    } deriving (Show)
+
+instance FromJSON InstallationToken where
+    parseJSON = withObject "installation token" $ \o -> InstallationToken
+        <$> ((encodeUtf8 :: Text -> ByteString) <$> o .: "token")
+        <*> (either fail pure =<< parseExpiresAt <$> o .: "expires_at")
+      where
+        parseExpiresAt :: String -> Either String UTCTime
+        parseExpiresAt = parseTimeM True defaultTimeLocale (iso8601DateFormat (Just "%H:%M:%SZ"))
 
 -- | Main installation auth info for application. 
 -- it containes immutable app information and app installation token
 -- which shoud be updated at least once per hour  
 data InstallationAuth = InstallationAuth 
-    {
-        _appId          :: !Int,           -- ^ application id 
-        _appPrivateKey  :: !PrivateKey,    -- ^ Private key to sign token requests  
-        _installationId :: !Text,          -- ^ Installation id 
-        _token          :: IORef GTAToken  -- ^ Installation Auth token. 
-    } 
+    {  _appId          :: !Int                -- ^ application id 
+    ,  _appPrivateKey  :: !PrivateKey         -- ^ Private key to sign token requests  
+    ,  _installationId :: !Text               -- ^ Installation id 
+    ,  _token          :: !(MVar InstallationToken)  -- ^ Installation Auth token. 
+    }
 
 makeLenses '' InstallationAuth
 
-instance FromJSON GTAToken where
-    parseJSON = withObject "Github installation token" $ \o -> do 
-        instToken   <- o .: "token"  
-        expireAtM   <- expPOSIXtime <$> o .: "expires_at"
-        case expireAtM of 
-            Nothing       -> fail "Failed to parse expiraion time."
-            Just expireAt -> return GTAToken{..}
-
--- | newtype for failed JSON Web token generation.
-newtype FailedGenerateJWT = FailedGenerateJWT Text
-
-instance Show.Show FailedGenerateJWT where
-    show (FailedGenerateJWT err) = toString err
-
-instance Exception FailedGenerateJWT
-
 -- | Checks if current app installation token is not expired. If it is not, than it just returns token 
 -- with OAuth wrapping. 
--- Otherwise it call function that gets new token from github and writes it to IORef, and also returns that new token. 
+-- Otherwise it call function that gets new token from github and writes it to IORef, 
+-- and also returns that new token. 
 authenticateInstallation :: InstallationAuth -> IO Auth
 authenticateInstallation instAuth = do 
-    gtaToken    <- readIORef (instAuth ^. token)
-    currentTime <- getPOSIXTime
-    if (expireAt gtaToken) - currentTime >= bufferTime 
-    then return $ OAuth $ encodeUtf8 $ instToken gtaToken
+    gtaToken    <- readMVar (instAuth ^. token)
+    currentTime <- getCurrentTime
+    if getExpirationTime gtaToken `diffUTCTime` currentTime >= bufferTime 
+    then return $ OAuth $ getToken gtaToken
     else do 
-        let applicationId = instAuth ^. appId
-        OAuth <$> encodeUtf8 <$> instToken <$> (join $ readIORef <$> renewInstAuthToken instAuth) 
+        updatedToken   <- takeMVar (instAuth ^. token)
+        renewInstAuthToken instAuth
+        updatedToken   <- readMVar (instAuth ^. token)
+        return $ OAuth $ getToken updatedToken
 
-
--- | Gets new token from github and writes it to IORef in given InstallationAuth. Also returns that token. 
-renewInstAuthToken :: InstallationAuth -> IO (IORef GTAToken) 
-renewInstAuthToken instAuth = do 
-    time    <- getPOSIXTime
-    let tkn = instAuth ^. token
-    case (makeJWT time (instAuth ^. appId) (instAuth ^. appPrivateKey)) of 
-        Left  err  -> throwM (FailedGenerateJWT err) 
-        Right jwt  -> do 
-            t <- request (https baseURL /: "installations" /: (instAuth ^. installationId) /: "access_tokens") mempty jwt    
-            writeIORef tkn t 
-            return tkn
-
--- | Creates new InstallationAuth value. Useing this function is the only way to create InstallationAuth value, 
--- because we don't export constructors. 
+-- | Creates new InstallationAuth value. Useing this function is the only way to create 
+-- InstallationAuth value, because we don't export constructors. 
 createInstAuth :: Int -> PrivateKey -> InstallationId -> IO InstallationAuth 
 createInstAuth appId key instId = do 
-    time    <- getPOSIXTime
-    case (makeJWT time appId key) of 
-        Left  err  -> throwM (FailedGenerateJWT err) 
-        Right jwt  -> do 
-            t      <- request (https baseURL /: "installations" /: instId /: "access_tokens") mempty jwt
-            token  <- newIORef t
-            return $ InstallationAuth appId key instId token
-    
+    token  <- newEmptyMVar
+    let instAuth = InstallationAuth appId key instId token
+    _      <- renewInstAuthToken instAuth
+    return instAuth
 
+-- | Gets new token from github and writes it to IORef in given InstallationAuth. 
+renewInstAuthToken :: InstallationAuth -> IO () 
+renewInstAuthToken instAuth = do 
+    time    <- getCurrentTime
+    let tkn = instAuth ^. token
+    let jwt = makeJWT time (instAuth ^. appId) (instAuth ^. appPrivateKey) 
+    t <- request (https baseURL /: "installations" /: (instAuth ^. installationId) /: "access_tokens") mempty jwt    
+    putMVar tkn t 
+    return ()
+    
 -- | Creates JSON Web Token for given application Id using application's privateKey. 
-makeJWT :: POSIXTime -> Int -> PrivateKey -> Either Text JSON 
-makeJWT currentTime appId appPrivateKey = do 
-    let currDate = numericDate currentTime 
-    let expDate  = numericDate (currentTime + jwtExpTime)
-    if (currDate >> expDate) == Nothing 
-    then Left "Failed to convert time to numericDate."
-    else do
-        let issuer = stringOrURI $ show appId 
-        if issuer == Nothing 
-        then Left $ "Failed to convert id " <> show appId <> " to StringOrURI type"
-        else do 
-            let jwtClaimsSet = mempty {iss = issuer, iat = currDate, exp = expDate}
-            return $ encodeSigned (RSAPrivateKey appPrivateKey) jwtClaimsSet
+makeJWT :: UTCTime -> Int -> PrivateKey -> JSON 
+makeJWT currentTime appId appPrivateKey = 
+  let currDate     = numericDate $ utcTimeToPOSIXSeconds currentTime 
+      expDate      = numericDate $ utcTimeToPOSIXSeconds $ jwtExpTime `addUTCTime` currentTime
+      issuer       = stringOrURI $ show appId 
+      jwtClaimsSet = mempty {iss = issuer, iat = currDate, exp = expDate}
+  in encodeSigned (RSAPrivateKey appPrivateKey) jwtClaimsSet
 
 -- | Make request to github to get installation Auth token. 
 request :: FromJSON m => Url scheme -> Option scheme -> JSON -> IO m
@@ -140,13 +118,6 @@ request url opts jwt = runReq def $ responseBody <$>
     jsonResponse -- specify how to interpret response
      (  header "Authorization" ("Bearer " <> encodeUtf8 jwt)
      <> header "Accept" "application/vnd.github.machine-man-preview+json"
-     <> header "user-agent" "req"
+     <> header "user-agent" "Haskell req-1.2.1"
      <> opts
      )
-
--- | It reads expiration time of token from github's format to POSIXTime.
-expPOSIXtime :: Text -> Maybe POSIXTime
-expPOSIXtime txt = 
-  let date = T.takeWhile (/='T') txt 
-      time = T.takeWhile (/='Z') $ T.takeWhileEnd (/='T') txt 
-  in  (utcTimeToPOSIXSeconds . fst) <$> (safeHead $ reads $ toString $ date <> " " <> time <> ".0 UTC")
