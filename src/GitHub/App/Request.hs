d97b97c67a77a76aeb15e76db7a6689dea21f542
Kirill Elagin <kirelagin@gmail.com>
2018-08-10 05:10:34 +0300

    Bring the codebase closer to github upstream
    * Use http-client instead of req
    * Provide `executeAppRequest`
    * Make MVar handling exception-safe

---
+++ src/GitHub/App/Request.hs
@@ -0,0 +1,38 @@
+{- This Source Code Form is subject to the terms of the Mozilla Public
+ - License, v. 2.0. If a copy of the MPL was not distributed with this
+ - file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ -}
+
+{-# LANGUAGE CPP #-}
+
+module GitHub.App.Request
+       ( executeAppRequest
+       , executeAppRequestWithMgr
+       ) where
+
+import GitHub.Data (Error (..))
+import GitHub.Data.Request (Request)
+import GitHub.Request (executeRequestWithMgr)
+import Network.HTTP.Client (Manager)
+import Network.HTTP.Client.TLS (newTlsManager)
+
+import GitHub.App.Auth (InstallationAuth, obtainAccessToken)
+
+
+executeAppRequest :: InstallationAuth -> Request k a -> IO (Either Error a)
+executeAppRequest instAuth req = do
+    manager <- newTlsManager
+    x <- executeAppRequestWithMgr manager instAuth req
+#if !MIN_VERSION_http_client(0, 4, 18)
+    closeManager manager
+#endif
+    pure x
+
+executeAppRequestWithMgr
+    :: Manager
+    -> InstallationAuth
+    -> Request k a
+    -> IO (Either Error a)
+executeAppRequestWithMgr mgr instAuth req = obtainAccessToken mgr instAuth >>= \case
+    Right auth -> executeRequestWithMgr mgr auth req
+    Left  err  -> pure $ Left err
